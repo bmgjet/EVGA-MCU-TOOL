c5464c62af21955777eff2c7c8f6c77bbb87a663
bmg <bmgjet@gmail.com>
2021-02-03 18:51:16 +1300

    initial release tested on 3090 XC3 and FTW3

---
+++ Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿
+namespace EVGAMCUTOOL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(282, 125);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(271, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Flash";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "MCU Firmware:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Enabled = false;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(186, 82);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(363, 33);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "GPU Index:";
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(186, 128);
+            this.maskedTextBox1.Mask = "0";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(90, 31);
+            this.maskedTextBox1.TabIndex = 4;
+            this.maskedTextBox1.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(137, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "PX1 Version:";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(186, 31);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(363, 33);
+            this.comboBox2.TabIndex = 6;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 178);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(112, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "ProductID:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(42, 219);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(91, 25);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Version:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(565, 259);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "EVGA MCU Tool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace EVGAMCUTOOL
+{
+    public partial class Form1 : Form
+    {
+		string EVGAFlash = @"C:\Program Files\EVGA\Precision X1\Library\FirmwareUpdate.exe";
+		string PX1Version = "";
+		string[] Firmwares;
+		string SelectedFirmware = "";
+
+		public Form1()
+        {
+            InitializeComponent();
+		}
+
+
+		private void SetFlashlocation()
+		{
+			MessageBox.Show("FirmwareUpdate.exe Not found!" + Environment.NewLine + "Please select this folder." + Environment.NewLine + @"Its normally in C:\Program Files\EVGA\Precision X1\Library\", "Select FirmwareUpdate.exe");
+			OpenFileDialog openFileDialog1 = new OpenFileDialog
+			{
+				DefaultExt = "exe",
+				Filter = "FirmwareUpdate.exe|FirmwareUpdate.exe",
+				RestoreDirectory = true,
+			};
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                EVGAFlash = openFileDialog1.FileName;
+            }
+			else
+            {
+				this.Close();
+            }
+		}
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+			try
+			{
+				if (!File.Exists(EVGAFlash))
+				{
+					SetFlashlocation();
+				}
+			}
+			catch
+            {
+				SetFlashlocation();
+			}
+			foreach (string file in Directory.GetDirectories("MCUFirmware"))
+			{
+				comboBox2.Items.Add(file.Replace(@"MCUFirmware\", ""));
+			}
+		}
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+			comboBox1.Items.Clear();
+			PX1Version = comboBox2.Text;
+			if (PX1Version != "")
+            {
+				comboBox1.Enabled = true;
+				Firmwares = File.ReadAllLines(@"MCUFirmware\" + PX1Version + @"\info.txt");
+
+				foreach (string file in Firmwares)
+				{
+					comboBox1.Items.Add(file.Split(',')[0]);
+				}
+				comboBox1.SelectedIndex = 0;
+			}
+		}
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+			int comboindex = comboBox1.SelectedIndex;
+			string[] firmwareinfo = Firmwares[comboindex].Replace(" ", "").Split(',');
+			label4.Text = "ProductID: " + ProductID(firmwareinfo[1]);
+			label5.Text = "Version: " + firmwareinfo[2] + "." + firmwareinfo[3];
+			SelectedFirmware = Directory.GetCurrentDirectory() + @"\MCUFirmware\" + PX1Version + @"\" + comboBox1.Text;
+			button1.Enabled = true;
+		}
+
+		public string ProductID (string id)
+        {
+			switch (id)
+            {
+				case "1":
+					return "E221_XC3";
+				case "2":
+					return "E227_FTW3";
+				case "3":
+					return "KINGPIN";
+				case "4":
+					return "E230_FTW3";
+				case "5":
+					return "E229_3Fans";
+				case "6":
+					return "XC3 HYBRID";
+				case "7":
+					return "XC3 HC";
+				case "8":
+					return "FTW3 HYBRID";
+				case "9":
+					return "FTW3 HC";
+				case "10":
+					return "KINGPIN HC";
+				default:
+					return id;
+			}
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+			if (SelectedFirmware != "")
+			this.UpdateFirmware(uint.Parse(maskedTextBox1.Text), SelectedFirmware, 0u, 0);
+		}
+
+		private void UpdateFirmware(uint gpuIndex, string fullname, uint address = 0u, byte version = 0)
+		{
+			string text = "--gpu " + gpuIndex.ToString() + " --fullname " + fullname +  " --return " + @"C:\Program Files\EVGA\Precision X1\PrecisionX_x64.exe";
+			if (address != 0u && version != 0)
+			{
+				text += string.Format(" --address 0x{0:X8} --version {1}", address, version);
+			}
+			string fileName = EVGAFlash;
+			try
+			{
+				Process.Start(fileName, text);
+			}
+			catch (Exception)
+			{
+			}
+		}
+
+	}
+}
